fd91951018bd49d890d3be8e91fde981088ef91c
Fernando Guillen <fguillen.mail@gmail.com>
2021-06-24 13:03:19 +0200

    Fixing Markdown rendering to prevent HTML code injection

--- config/initializers/markdown_renderer.rb
+++ config/initializers/markdown_renderer.rb
@@ -2,17 +2,17 @@ module MarkdownRenderer
   def self.markdown
     @@markdown ||=
       Redcarpet::Markdown.new(
-        Redcarpet::Render::HTML,
+        Redcarpet::Render::HTML.new(
+          escape_html: true,
+          hard_wrap: true,
+          safe_links_only: true,
+          with_toc_data: true
+        ),
         autolink: true,
-        # escape_html: true,
         fenced_code_blocks: true,
-        filter_html: true,
-        hard_wrap: true,
         no_intra_emphasis: true,
-        prettify: true,
         space_after_headers: true,
-        tables: true,
-        with_toc_data: true
+        tables: true
       )
   end
 
