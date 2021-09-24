6eed30c86c3bd1058a281a2a8ec48d945d6e2947
Fernando Guillen <fguillen.mail@gmail.com>
2021-09-24 20:19:32 +0200

    Removing SECRET_HOSTS

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   default from: APP_CONFIG[:admin_email]
 
   def admin_user_reset_password(admin_user)
-    @reset_password_link = admin_reset_password_url(admin_user.perishable_token, host: APP_CONFIG[:hosts].first)
+    @reset_password_link = admin_reset_password_url(admin_user.perishable_token, host: APP_CONFIG[:host])
 
     mail(
       to: admin_user.email,
@@ -11,7 +11,7 @@ class Notifier < ActionMailer::Base
   end
 
   def front_user_reset_password(front_user)
-    @reset_password_link = front_reset_password_url(front_user.perishable_token, host: APP_CONFIG[:hosts].first)
+    @reset_password_link = front_reset_password_url(front_user.perishable_token, host: APP_CONFIG[:host])
 
     mail(
       to: front_user.email,
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,5 @@ Rails.application.configure do
   end
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts = ENV.fetch('SECRET_HOSTS').split(",")
-
   config.active_storage.service = :amazon
 end
