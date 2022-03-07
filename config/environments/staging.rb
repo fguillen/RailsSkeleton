f8a21670763d0108ff2b410557f03250340a1e04
Fernando Guillen <fguillen.mail@gmail.com>
2022-03-07 20:26:45 +0100

    Rails upgrade task

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,25 +1,28 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.eager_load_paths += ["#{config.root}/lib"]
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.assets.compile = false
+  config.active_storage.service = :amazon
   config.log_level = :info
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
+  config.active_support.report_deprecations = false
   config.log_formatter = ::Logger::Formatter.new
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new($stdout)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :amazon
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV["SECRET_SENDGRID_API_KEY"],
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_173815) do
+ActiveRecord::Schema[6.1].define(version: 2021_11_25_173815) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
